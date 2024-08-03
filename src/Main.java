1d390909735a69325c233410b99354ecc1b3dfb2
Jacob Stone <jumboshrimp9554@gmail.com>
2024-08-03 18:24:16 -0400

    created the task class, representing a task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc =  new Scanner(System.in);
+        int continueLoop = 1;
+
+        while (continueLoop != 0) {
+            System.out.println("Welcome to the ToDo App!");
+            System.out.println("Enter 1 to continue! \nEnter 0 to exit!");
+            System.out.print("Please make a selection: ");
+            continueLoop = sc.nextInt();
+        }
+
+        System.out.print("Goodbye!");
     }
 }
---
+++ src/Task.java
@@ -0,0 +1,124 @@
+public class Task {
+    private String title;
+    private String descr;
+    private String dueDate;
+    private String category;
+    private boolean isCompleted;
+
+    //default constructor
+    public Task() {
+        title = "";
+        descr = "";
+        dueDate = "";
+        category = "";
+        isCompleted = false;
+    }
+
+    //overloaded constructor to assign tasks
+
+    /**
+     *
+     * @param title the title of the task
+     * @param descr the description of the task
+     * @param dueDate the date in which the task is due
+     * @param category the category of the task
+     */
+    public Task(String title, String descr, String dueDate, String category) {
+        this.title = title;
+        this.descr = descr;
+        this.dueDate = dueDate;
+        this.category = category;
+        this.isCompleted = false; //your task is not going to be done when you are creating it, this needs to be false
+    }
+
+    //getters
+
+    /**
+     *
+     * @return the title of the task
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     *
+     * @return the description of the task
+     */
+    public String getDescr() {
+        return descr;
+    }
+
+    /**
+     *
+     * @return the due date of the task
+     */
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    /**
+     *
+     * @return the category of the task
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     *
+     * @return get the completion status of the task
+     */
+    public boolean getIsCompleted() {
+        return isCompleted;
+    }
+
+    //setters
+
+    /**
+     *
+     * @param title the new title of the task
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     *
+     * @param descr the new description of the task
+     */
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    /**
+     *
+     * @param dueDate the new due date of the task
+     */
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    /**
+     *
+     * @param category the new category of the task
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     *
+     * @param isCompleted the new completion status of the task
+     */
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    /**
+     * Format and print the details of the task
+     */
+    public void printDetails() {
+
+    }
+}
