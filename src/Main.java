4eb9678d18e0e737ae2127e0f980de0ace723415
Jacob Stone <jumboshrimp9554@gmail.com>
2024-08-03 22:26:18 -0400

    worked on functionality for editing a task

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,12 @@ public class Main {
 
         while (continueLoop != 0) {
             System.out.println("Welcome to the ToDo App!");
-            System.out.println("Enter 1 to create a new task! \nEnter 2 to view existing tasks! \nEnter 0 to exit!");
+            System.out.println("Enter 1 to create a new task! \nEnter 2 to view existing tasks! \nEnter 3 to edit an existing task! \nEnter 0 to exit!");
             System.out.print("Please make a selection: ");
             continueLoop = sc.nextInt();
             System.out.print("\n");
 
+            //process menu selections
             if(continueLoop == 1) {
                 //call method to create task
                 task = createTask(sc);
@@ -20,20 +21,27 @@ public class Main {
             else if (continueLoop == 2) {
                 task.printDetails();
             }
+            else if(continueLoop == 3) {
+                //call method to edit task
+                task = editTask(sc, task);
+            }
             System.out.print("\n");
         }
 
         System.out.print("Goodbye!");
     }
 
+    /**
+     *
+     * @param sc the Scanner used to receive input from the user
+     * @return Task t, a task populated by user input
+     */
     public static Task createTask(Scanner sc) {
         Task t = new Task(); //create a task to populate
         String temp = ""; // a string that will hold the values passed into the task
 
         //clear the buffer of the scanner (if present)
-        if(sc.hasNextLine()) {
-            sc.nextLine();
-        }
+        clearBuffer(sc);
 
         System.out.print("Enter the name of your task: ");
         temp = sc.nextLine();
@@ -52,7 +60,77 @@ public class Main {
         t.setCategory(temp);
 
         t.setCompleted(false);
+        temp = "";
 
         return t;
+    }
+
+    public static Task editTask(Scanner sc, Task task) {
+        Task t = task;
+        int choice;
+        String temp;
+
+        //clear the buffer of the scanner (if present)
+        clearBuffer(sc);
+
+        System.out.print("Enter 1 to edit task name! \nEnter 2 to edit task description! \nEnter 3 to edit task due date!" +
+                "\nEnter 4 to edit task category! \nEnter 5 to edit task completion status! \nEnter 6 to cancel!");
+        choice = sc.nextInt();
+
+        while(choice != 6) {
+            clearBuffer(sc);
+
+            if(choice == 1) {
+                //edit name
+                System.out.print("Enter new task title: ");
+                temp = sc.nextLine();
+                t.setTitle(temp);
+            }
+            else if(choice == 2) {
+                //edit description
+                System.out.print("Enter new task description: ");
+                temp = sc.nextLine();
+                t.setDescr(temp);
+            }
+            else if(choice == 3) {
+                //edit due date
+                System.out.print("Enter new task due date: ");
+                temp = sc.nextLine();
+                t.setDueDate(temp);
+            }
+            else if(choice == 4) {
+                //edit category
+                System.out.println("Enter new task category: ");
+                temp = sc.nextLine();
+                t.setCategory(temp);
+            }
+            else if(choice == 5) {
+                //edit completion
+                System.out.print("Enter new completion status (type yes or no)");
+                temp = sc.nextLine();
+                if(temp.toLowerCase().equals("yes")){
+                    t.setCompleted(true);
+                }
+                else if(temp.toLowerCase().equals("no")) {
+                    t.setCompleted(false);
+                }
+                else {
+                    System.out.println("ERROR! UNKNOWN COMPLETION DESIGNATION! Please try again.");
+                }
+            }
+            else {
+                System.out.println("ERROR! INVALID CHOICE!");
+            }
+
+            System.out.print("Enter 1 to edit task name! \nEnter 2 to edit task description! \nEnter 3 to edit task due date!" +
+                    "\nEnter 4 to edit task category! \nEnter 5 to edit task completion status! \nEnter 6 to cancel!");
+            choice = sc.nextInt();
+        }
+
+        return t;
+    }
+
+    public static void clearBuffer(Scanner sc) {
+        sc.nextLine();
     }
 }
